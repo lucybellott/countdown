b0336168168698664b721c4248c109fa73e84301
Lucy <lucybellott@gmail.com>
2023-11-14 13:17:07 -0500

    fibonacci

--- fibonacci.py
+++ fibonacci.py
@@ -5,3 +5,21 @@ def fibonacci(n):
         fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
 
     return fib_sequence[:n]
+
+##################
+
+def fibonacci_recursive(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    else:
+        fib_sequence = fibonacci_recursive(n - 1)
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+        return fib_sequence
+# Example usage:
+n = 10
+result = fibonacci_recursive(n)
+print(f"The first {n} numbers in the Fibonacci sequence are: {result}")
